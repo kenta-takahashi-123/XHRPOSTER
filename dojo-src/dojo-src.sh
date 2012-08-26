5a158b5179361b05eea8577ebe12f49bdbd58556
rika-t <agigigigi@gmail.com>
2012-08-26 12:06:29 +0900

    create directory

---
+++ dojo-src/dojo-src.sh
@@ -0,0 +1,5 @@
+git svn clone http://svn.dojotoolkit.org/src/branches/1.8/demos
+git svn clone http://svn.dojotoolkit.org/src/branches/1.8/dijit
+git svn clone http://svn.dojotoolkit.org/src/branches/1.8/dojo
+git svn clone http://svn.dojotoolkit.org/src/branches/1.8/dojox
+git svn clone http://svn.dojotoolkit.org/src/branches/1.8/util
